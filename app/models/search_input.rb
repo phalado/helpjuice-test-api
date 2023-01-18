e65af88bffb17ab5fda5ac71831f4bce7ed7fa1e
Rapha <phalado@gmail.com>
2023-01-18 12:23:21 -0300

    Some corrections to elastic searchs quer

--- app/models/search_input.rb
+++ app/models/search_input.rb
@@ -4,7 +4,7 @@ class SearchInput < ApplicationRecord
   searchkick
 
   def self.fetch_input_count
-    SearchInput.search('*', body_options: { aggs: { input: { terms: { field: "input" } } } })
+    SearchInput.search('*', body_options: { aggs: { input: { terms: { field: "input", size: 100 } } } })
                .aggs["input"]["buckets"]
   end
 end
